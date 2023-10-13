bea04fe9e5142b1e8f5d9819016e8384f0778e16
TheoCourtin <theo.joseph.courtin@gmail.com>
2023-10-13 08:50:11 +0200

    adjustement

--- front-end/src/pages/SignIn.js
+++ front-end/src/pages/SignIn.js
@@ -1,7 +1,40 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { getToken, getUser } from "../feature/user.slice";
+import CallAPI from "../services/CallAPI";
+
+/**
+ * Returns a React component displays the signin page
+ * @returns React Component
+ */
 
 const SignIn = () => {
+  const [email, setEmail] = useState("tony@stark.com");
+  const [password, setPassword] = useState("password123");
+  const validToken = useSelector((state) => state.user.token);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const setUser = async () => {
+    const data = await CallAPI.getUserInfo(validToken);
+    dispatch(getUser({ firstName: data.firstName, lastName: data.lastName }));
+  };
+
+  const handleSubmit = async () => {
+    const res = await CallAPI.tokenLogin({ email, password });
+    if (res) {
+      dispatch(getToken({ token: res, email: email }));
+    }
+  };
+
+  useEffect(() => {
+    if (validToken) {
+      setUser();
+      navigate("/user");
+    }
+  });
+
   return (
     <main className="sign-in main bg-dark">
       <section className="sign-in-content">
@@ -11,24 +44,33 @@ const SignIn = () => {
 
         <form>
           <div className="input-wrapper">
-            <label htmlFor="username">Username</label>
-            <input type="text" id="username" />
+             <label htmlFor="username">Email</label>
+            <input
+              type="text"
+              id="username"
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+            />
           </div>
           <div className="input-wrapper">
             <label htmlFor="password">Password</label>
-            <input type="password" id="password" />
+            <input
+              type="password"
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+            />
           </div>
           <div className="input-remember">
             <input type="checkbox" id="remember-me" />
             <label htmlFor="remember-me">Remember me</label>
           </div>
-          {/* <!-- PLACEHOLDER DUE TO STATIC SITE --> */}
-          <Link to="/user" className="sign-in-button">
+          <button
+            type="button"
+            className="sign-in-button"
+            onClick={() => handleSubmit()}
+          >
             Sign In
-          </Link>
-          {/* <!-- SHOULD BE THE BUTTON BELOW --> */}
-          {/* <!-- <button class="sign-in-button">Sign In</button> --> */}
-          {/* <!--  --> */}
+            </button>
         </form>
       </section>
     </main>
--- front-end/src/pages/User.js
+++ front-end/src/pages/User.js
@@ -1,36 +1,109 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import Account from "../components/Account";
+import { getUser } from "../feature/user.slice";
+import CallAPI from "../services/CallAPI";
 
+/**
+ * Returns a React component displays the user page
+ * @returns React Component
+ */
 const User = () => {
-  return (
-    <main className="main bg-dark">
-      <div className="header">
-        <h1>
-          Welcome back
-          <br />
-          Tony Jarvis!
-        </h1>
-        <button className="edit-button">Edit Name</button>
-      </div>
-      <h2 className="sr-only">Accounts</h2>
-      <Account
-        title="Argent Bank Checking (x8349)"
-        amount="$2,082.79"
-        description="Available Balance"
-      />
-      <Account
-        title="Argent Bank Savings (x6712)"
-        amount="$10,928.42"
-        description="Available Balance"
-      />
-      <Account
-        title="Argent Bank Credit Card (x8349)"
-        amount="$184.30"
-        description="Current Balance"
-      />
+  const [editContent, setEditContent] = useState(false);
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const user = useSelector((state) => state.user);
+  const validToken = useSelector((state) => state.user.token);
+  const dispatch = useDispatch();
 
-    </main>
-  );
+  useEffect(() => {
+    setFirstName(user.firstName);
+    setLastName(user.lastName);
+  }, [user]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await CallAPI.setUserInfo(validToken, { firstName, lastName });
+    dispatch(getUser({ firstName: firstName, lastName: lastName }));
+    setEditContent(false);
+  };
+
+  if (validToken) {
+    return (
+      <main className="user main bg-dark">
+        <div className="header">
+          {editContent ? (
+            <>
+              <h1>Welcome back</h1>
+              <form className="profil-form">
+                <div className="profil-inputs">
+                  <input
+                    type="text"
+                    name="firstName"
+                    className="profil-input"
+                    onChange={(e) => setFirstName(e.target.value)}
+                    value={firstName}
+                  />
+                  <input
+                    type="text"
+                    name="lastName"
+                    className="profil-input"
+                    onChange={(e) => setLastName(e.target.value)}
+                    value={lastName}
+                  />
+                </div>
+                <div className="profil-buttons">
+                  <button
+                    className="button-valid"
+                    onClick={(e) => handleSubmit(e)}
+                  >
+                    Save
+                  </button>
+                  <button
+                    className="button-cancel"
+                    onClick={() => setEditContent(false)}
+                  >
+                    Cancel
+                  </button>
+                </div>
+              </form>
+            </>
+          ) : (
+            <>
+              <h1>
+                Welcome back
+                <br />
+                {user.firstName} {user.lastName} !
+              </h1>
+              <button
+                className="edit-button"
+                onClick={() => setEditContent(true)}
+              >
+                Edit Name
+              </button>
+            </>
+          )}
+        </div>
+        <h2 className="sr-only">Accounts</h2>
+        <Account
+          title="Argent Bank Checking (x8349)"
+          amount="$2,082.79"
+          description="Available Balance"
+        />
+        <Account
+          title="Argent Bank Savings (x6712)"
+          amount="$10,928.42"
+          description="Available Balance"
+        />
+        <Account
+          title="Argent Bank Credit Card (x8349)"
+          amount="$184.30"
+          description="Current Balance"
+        />
+      </main>
+    );
+  }
+  //si il n'est pas connecté, return vers signin
 };
 
 export default User;
